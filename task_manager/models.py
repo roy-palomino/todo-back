7cba3819b884e5af6977a58fea9b0657143652d3
Roy Palomino Rojas <roy100995@gmail.com>
2024-07-25 17:46:06 -0500

    refactor: change Meta to show most recent tasks first

--- task_manager/models.py
+++ task_manager/models.py
@@ -48,7 +48,7 @@ class Task(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['created_at']
+        ordering = ['-created_at']
 
     def __str__(self):
         return self.name
