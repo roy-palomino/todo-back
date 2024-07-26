34151ff3164bd7abc8a895ff0330e5b332ef1cf8
Roy Palomino Rojas <roy100995@gmail.com>
2024-07-26 18:20:26 -0500

    feat: add cutomized Views to manage pair tokens
    - change refresh token nature to be http-only cookie

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import User
+from django.conf import settings
 
-from rest_framework import viewsets, permissions, generics
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework import viewsets, permissions, generics, views
+from rest_framework.response import Response
 
 from .models import Category, Tag, Task
-
 from .serializers import (
     UserSerializer,
     UserRegistrationSerializer,
@@ -12,6 +14,47 @@ from .serializers import (
     TaskReadSerializer,
     TaskWriteSerializer,
 )
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        refresh_token = response.data.get('refresh')
+        response.set_cookie(
+            key=settings.SIMPLE_JWT['AUTH_COOKIE'],
+            value=refresh_token,
+            httponly=True,
+            secure=True,
+            samesite='Lax'
+        )
+        del response.data['refresh']
+        return response
+
+
+class CustomTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        request.data['refresh'] = request.COOKIES.get(settings.SIMPLE_JWT['AUTH_COOKIE'])
+        response = super().post(request, *args, **kwargs)
+        if 'refresh' in response.data:
+            response.set_cookie(
+                key=settings.SIMPLE_JWT['AUTH_COOKIE'],
+                value=response.data['refresh'],
+                httponly=True,
+                secure=True,
+                samesite='Lax'
+            )
+            del response.data['refresh']
+        return response
+
+
+class LogoutView(views.APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request):
+        response = Response()
+        response.delete_cookie(settings.SIMPLE_JWT['AUTH_COOKIE'])
+        response.status_code = 204
+        return response
 
 
 class UserViewset(viewsets.ReadOnlyModelViewSet):
