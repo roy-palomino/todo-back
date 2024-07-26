e7a473ec3d81acb74e34ced925f89fad9c733928
Roy Palomino Rojas <roy100995@gmail.com>
2024-07-26 18:19:43 -0500

    refactor: change jwt token lifetime
    - access token set to 5 min
    - refresh token set to 60 days

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -91,8 +91,13 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=60),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=360),
+    "AUTH_COOKIE": "refresh_token",
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=60),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "AUTH_HEADER_TYPES": ('Bearer',),
+    "AUTH_TOKEN_CLASSES": ('rest_framework_simplejwt.tokens.AccessToken',),
 }
 
 REST_AUTH = {
